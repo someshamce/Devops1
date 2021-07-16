d7bb97cd5310a0df5108c5890d9b93722a33a4d3
Ubuntu <ubuntu@ip-172-31-5-155.ap-south-1.compute.internal>
2021-07-16 18:09:19 +0000

    mail.sh edited

--- mail.sh
+++ mail.sh
@@ -3,5 +3,5 @@
 disk=`df -h . | awk -F " " 'NR>1 {print $(NF-1)}' | sed 's/%//g'`
 if [ $disk -gt 70 ]
 then
-	mail -s "Memory reached 70 percent" someshamce@gmail.com
+	mail -s "Memory reached 70 percent" madhu.siddaramanna93@gmail.com
 fi
